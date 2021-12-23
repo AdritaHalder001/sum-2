622af70a9a98d7fa0b4ec89f639f410f4a9b2dfe
root <root@localhost>
2021-12-23 21:13:40 +0600

    auto publish 1640272419933

---
+++ src/main.py
@@ -0,0 +1,5 @@
+print("Hey Abida, I am your programming calculator(only used for addition and subtraction).")
+
+print("Input you sum and then click enter:")
+print('=',eval(str(input())))
+print("Thank you my gf for using me:D")
